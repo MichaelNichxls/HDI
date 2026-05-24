4a8556eb9e550fba0df791b1d6fb34d81ab70699
Michael Nichols <mjnichols10@yahoo.com>
2026-05-24 16:50:23 -0500

    Now properly handles data and iframes

--- cba-boxscores/boxscores.py
+++ cba-boxscores/boxscores.py
@@ -21,7 +21,7 @@ from typing import Any
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from playwright.sync_api import BrowserContext, Locator, Page, TimeoutError, sync_playwright
+from playwright.sync_api import BrowserContext, FrameLocator, Locator, Page, TimeoutError, sync_playwright
 
 LOGGER = logging.getLogger(__name__)
 NOW = datetime.now()
@@ -103,7 +103,7 @@ PARENS_PATTERN = re.compile(r"\([^)]*\)")
 NUMBER_PATTERN = re.compile(r"#?\d{1,2}")
 LAST_FIRST_PATTERN = re.compile(r"(\b.*),\s*(.*\b)")
 
-type PageOrLocatorToLocator = Callable[[Page | Locator], Locator]
+type PageOrLocatorToLocator = Callable[[Page | Locator | FrameLocator], Locator]
 
 META_GAME_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("table.schedule-event-table__table tbody > tr").or_(
     locator.locator("table.sidearm-calendar-table tbody > tr")
@@ -122,29 +122,25 @@ GAME_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("h2.roste
     locator.locator("header.sidearm-box-score-header h3")
 )
 DATE_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("time.match-stats-header__match-details-item-value").or_(locator.locator("dt:has-text('Date') + dd"))
-TEAM_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("xpath=thead[contains(@class,'advanced-table__thead')]/tr[1]/th[last()]").or_(
-    locator.locator(f"xpath=self::table[@id='{locator.get_attribute('id')}']/caption")
-)
-PLAYER_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("xpath=tbody[contains(@class,'advanced-table__tbody')]/tr[normalize-space(td[1])!='P']").or_(
-    locator.locator(f"xpath=self::table[@id='{locator.get_attribute('id')}']/tbody/tr")
-)
-NAME_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator(
-    "xpath=td[count(ancestor::table//th[normalize-space()='Player']/preceding-sibling::th)+1][contains(@class,'advanced-table__cell')]"
-).or_(locator.locator("xpath=th[ancestor::tbody/preceding-sibling::thead//th[contains(@aria-controls,'DataTables_Table_')]]"))
 BATTING_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("//*[normalize-space()='Hitting']/following-sibling::*[1]//table").or_(
     locator.locator("//h3[normalize-space()='Batting']/following-sibling::*//table")
 )
 PITCHING_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("//*[normalize-space()='Pitching']/following-sibling::*[1]//table").or_(
     locator.locator("//h3[normalize-space()='Pitching']/following::table[position()<=2]")
 )
+BP_TEAM_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("thead tr:first-child th.advanced-table__cell--head:last-child").or_(locator.locator("> caption"))
+BP_PLAYER_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator("tbody > tr")
+BP_PLAYER_NAME_LOCATOR: PageOrLocatorToLocator = lambda locator: locator.locator(
+    "xpath=td[count(ancestor::table//th[normalize-space()='Player']/preceding-sibling::th)+1][contains(@class,'advanced-table__cell')]"
+).or_(locator.locator("xpath=th[ancestor::tbody/preceding-sibling::caption]"))
 
 
 def normalize(locator: Locator) -> str:
-    str_ = locator.inner_text()
+    str_ = locator.inner_text(timeout=5_000)
     str_ = PARENS_PATTERN.sub("", str_)
     str_ = NUMBER_PATTERN.sub("", str_)
     str_ = LAST_FIRST_PATTERN.sub(r"\2 \1", str_)
-    return str_.replace("\n", " ").strip().title()
+    return " ".join(str_.replace("\n", " ").strip().title().split())
 
 
 # def get_boxscore_series(conference, metadata: dict[str, Any]) -> dict[str, Any]:
@@ -180,19 +176,23 @@ def get_boxscore_metadata(context: BrowserContext, url_: str) -> Generator[dict[
 
 
 # series = ((date - SERIES[conference]).days + 1) // 7 + 1
-# metadata: dict[str, Any]
 def get_boxscores(context: BrowserContext, url: str) -> Generator[dict[str, str], None, None]:
     with context.new_page() as page:
         page.set_default_timeout(10_000)
         page.goto(url)
+        try:
+            page_or_frame = page.frame_locator(".iframe-container__iframe").frame_locator("#boxscore")
+            game = normalize(GAME_LOCATOR(page_or_frame))
+        except TimeoutError:
+            page_or_frame = page
+            game = normalize(GAME_LOCATOR(page_or_frame))
 
-        game = normalize(GAME_LOCATOR(page))
-        date = datetime.strptime(DATE_LOCATOR(page).inner_text(), "%m/%d/%Y")
-        for batting in BATTING_LOCATOR(page).all():
-            team = normalize(TEAM_LOCATOR(batting))
-            for player in PLAYER_LOCATOR(batting).all():
-                data = player.locator("td").all()
-                name = normalize(NAME_LOCATOR(player))
+        date = datetime.strptime(DATE_LOCATOR(page_or_frame).inner_text(), "%m/%d/%Y")
+        for batting in BATTING_LOCATOR(page_or_frame).all():
+            team = normalize(BP_TEAM_LOCATOR(batting))
+            for player in BP_PLAYER_LOCATOR(batting).all():
+                data = player.locator("td, th").all()
+                name = normalize(BP_PLAYER_NAME_LOCATOR(player))
                 position = data[0].inner_text()
                 yield {
                     "game": game,
@@ -200,14 +200,13 @@ def get_boxscores(context: BrowserContext, url: str) -> Generator[dict[str, str]
                     "team": team,
                     "player": name,
                     "position": position,
-                    # FIXME
                     **{s: data[i].inner_text() for i, s in enumerate(STATS["batting"], 2)},
                 }
-        for pitching in PITCHING_LOCATOR(page).all():
-            team = normalize(TEAM_LOCATOR(pitching))
-            for player in PLAYER_LOCATOR(pitching).all():
-                data = player.locator("td").all()
-                name = normalize(NAME_LOCATOR(player))
+        for pitching in PITCHING_LOCATOR(page_or_frame).all():
+            team = normalize(BP_TEAM_LOCATOR(pitching))
+            for player in BP_PLAYER_LOCATOR(pitching).all():
+                data = player.locator("td, th").all()
+                name = normalize(BP_PLAYER_NAME_LOCATOR(player))
                 yield {
                     "game": game,
                     "date": date,
@@ -216,76 +215,6 @@ def get_boxscores(context: BrowserContext, url: str) -> Generator[dict[str, str]
                     "position": "P",
                     **{s: data[i].inner_text() for i, s in enumerate(STATS["pitching"], 1)},
                 }
-
-
-#             wait.until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "iframe-container__iframe")))
-#             wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "boxscore")))
-
-#             game = normalize(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h2.roster-title__header:has(.roster-title__content)"))).text)
-#             date = datetime.strftime(metadata["date"], "%m/%d/%Y")
-
-#             for batting in wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[normalize-space()='Hitting']/following-sibling::*[1]//table"))):
-#                 team = normalize(batting.find_element(By.XPATH, "thead/tr[1]/th[last()]").text)
-
-#                 for player in batting.find_elements(By.XPATH, "tbody/tr[normalize-space(td[1])!='P']"):
-#                     data = player.find_elements(By.TAG_NAME, "td")
-#                     name = normalize(data[1].text)
-#                     position = data[0].text
-#                     yield {
-#                         "game": game,
-#                         "date": date,
-#                         "team": team,
-#                         "player": name,
-#                         "position": position,
-#                         **{s: data[i].text for i, s in enumerate(STATS["batting"], 2)},
-#                     }
-#             for pitching in wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[normalize-space()='Pitching']/following-sibling::*[1]//table"))):
-#                 team = normalize(pitching.find_element(By.XPATH, "thead/tr[1]/th[last()]").text)
-
-#                 for player in pitching.find_elements(By.XPATH, "tbody/tr"):
-#                     data = player.find_elements(By.TAG_NAME, "td")
-#                     name = normalize(data[0].text)
-#                     yield {
-#                         "game": game,
-#                         "date": date,
-#                         "team": team,
-#                         "player": name,
-#                         "position": "P",
-#                         **{s: data[i].text for i, s in enumerate(STATS["pitching"], 1)},
-#                     }
-#         elif metadata["conference"] == "acc":
-#             game = normalize(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "header.sidearm-box-score-header h3"))).text)
-#             date = datetime.strftime(metadata["date"], "%m/%d/%Y")
-
-#             for batting in wait.until(EC.presence_of_all_elements_located((By.XPATH, "//h3[normalize-space()='Batting']/following-sibling::*//table"))):
-#                 team = normalize(batting.find_element(By.TAG_NAME, "caption").text)
-
-#                 for player in batting.find_elements(By.XPATH, "tbody/tr[normalize-space(td[1])!='p']"):
-#                     data = player.find_elements(By.TAG_NAME, "td")
-#                     name = normalize(player.find_element(By.TAG_NAME, "th").text)
-#                     position = data[0].text
-#                     yield {
-#                         "game": game,
-#                         "date": date,
-#                         "team": team,
-#                         "player": name,
-#                         "position": position,
-#                         **{s: data[i].text for i, s in enumerate(STATS["batting"], 1)},
-#                     }
-#             for pitching in wait.until(EC.presence_of_all_elements_located((By.XPATH, "//h3[normalize-space()='Pitching']/following::table[position()<=2]"))):
-#                 team = normalize(pitching.find_element(By.TAG_NAME, "caption").text)
-
-#                 for player in pitching.find_elements(By.XPATH, "tbody/tr"):
-#                     data = player.find_elements(By.TAG_NAME, "td")
-#                     name = normalize(player.find_element(By.TAG_NAME, "th").text)
-#                     yield {
-#                         "game": game,
-#                         "date": date,
-#                         "team": team,
-#                         "player": name,
-#                         "position": "P",
-#                         **{s: data[i].text for i, s in enumerate(STATS["pitching"])},
-#                     }
 
 
 def main() -> None:
@@ -309,12 +238,9 @@ def main() -> None:
         p.chromium.launch() as browser,
         browser.new_context(viewport={"width": 1920, "height": 1080}) as context,
     ):
-        [
-            *get_boxscores(
-                context,
-                "https://theacc.com/boxscore.aspx?id=7ltyipP022aUJKmSgYVs3wXi7up73ghniwD5ElWyd07J7BNseBaMQePf0fY%2f3yUPZA5IMqdIE5VmHRIZ6WJS%2bkyPpdO7TG99p99nvkXg7X%2fJJBuClmPC0Wa46u%2f1jvqlqy2AB%2bcUj9MqBMw1weyv5FyxROrMcI%2fPV2Qw7YKgTzE%3d&path=baseball",
-            )
-        ]
+        # TODO: no position: P
+        for boxscore in get_boxscores(context, "https://www.secsports.com/boxscore/29842"):
+            pass
         # for series, metadata_by_series in groupby(get_boxscore_metadata(context, URLS[args.conference]), key=lambda m: m["series"]):
         #     if series not in range(1, 11):
         #         continue
